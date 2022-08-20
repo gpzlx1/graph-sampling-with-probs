d79e7e33c613c7a0219a38bc538da6dadbda434e
Ping Gong <gpzlx1@mail.ustc.edu.cn>
2022-08-20 22:56:39 +0800

    format python code

--- test/test_rowwise_sampling_cdf.py
+++ test/test_rowwise_sampling_cdf.py
@@ -1,12 +1,15 @@
 import os
 import torch
-so_path = os.path.join("/home/gpzlx1/graph_sampling_with_probs/build", 'libgswp.so')
+
+so_path = os.path.join("/home/gpzlx1/graph_sampling_with_probs/build",
+                       'libgswp.so')
 torch.ops.load_library(so_path)
 
-indptr = torch.tensor([0, 10 , 11, 20, 22, 22]).long().cuda()
+indptr = torch.tensor([0, 10, 11, 20, 22, 22]).long().cuda()
 indices = torch.arange(0, 22).long().cuda()
 seeds = torch.tensor([2, 3, 1, 4]).long().cuda()
 probs = torch.rand(22).float().abs().cuda()
 
-for i in torch.ops.gswp.RowWiseSamplingProb_CDF(seeds, indptr, indices, probs, 5, True):
+for i in torch.ops.gswp.RowWiseSamplingProb_CDF(seeds, indptr, indices, probs,
+                                                5, True):
     print(i)
